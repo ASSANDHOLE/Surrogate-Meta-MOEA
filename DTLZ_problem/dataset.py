add normalization to target value to fix gradient explosion

DTLZ_problem/dataset.py
@@ -99,7 +99,7 @@ def create_dataset_inner(x, n_dim: Tuple[int, int], delta: Tuple[List[int], List
     return new_x, y
 
 
-def create_dataset(problem_dim: Tuple[int, int], x=None, n_problem=None, spt_qry=None, delta=None) -> Tuple[
+def create_dataset(problem_dim: Tuple[int, int], x=None, n_problem=None, spt_qry=None, delta=None, normalize_targets=True, **_) -> Tuple[
     Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray],
     Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]
 ]:
@@ -117,6 +117,8 @@ def create_dataset(problem_dim: Tuple[int, int], x=None, n_problem=None, spt_qry
         [n_train, n_test]
     spt_qry : Tuple[int, int]
         The number of support and query points for each problem
+    normalize_targets : bool
+        Whether to normalize the targets
 
     Returns
     -------
@@ -148,9 +150,20 @@ def create_dataset(problem_dim: Tuple[int, int], x=None, n_problem=None, spt_qry
                 delta2 = np.random.randint(0, 10, n_problem[i])
                 delta.append([delta1, delta2])
 
-    train_set = (*create_dataset_inner(x[0], problem_dim, delta[0]), *create_dataset_inner(x[1], problem_dim, delta[1]))
-    test_set = (*create_dataset_inner(x[2], problem_dim, delta[2]), *create_dataset_inner(x[3], problem_dim, delta[3]))
-    return train_set, test_set
+    train_set = [*create_dataset_inner(x[0], problem_dim, delta[0]), *create_dataset_inner(x[1], problem_dim, delta[1])]
+    test_set = [*create_dataset_inner(x[2], problem_dim, delta[2]), *create_dataset_inner(x[3], problem_dim, delta[3])]
+    if normalize_targets:
+        minimum = np.min(np.concatenate([train_set[1], test_set[1]]), axis=None)
+        train_set[1] -= minimum
+        test_set[1] -= minimum
+        train_set[3] -= minimum
+        test_set[3] -= minimum
+        maximum = np.max(np.concatenate([train_set[1], test_set[1]]), axis=None)
+        train_set[1] /= maximum
+        test_set[1] /= maximum
+        train_set[3] /= maximum
+        test_set[3] /= maximum
+    return tuple(train_set), tuple(test_set)
 
 
 def test():

example.py
@@ -9,13 +9,13 @@ def get_args():
     args = NamedDict()
     args.problem_dim = (8, 3)
     args.train_test = (20, 3)
-    args.epoch = 300
-    args.update_lr = 0.00005
+    args.epoch = 100
+    args.update_lr = 0.01
     args.meta_lr = 0.001
     args.k_spt = 20
     args.k_qry = 100
-    args.update_step = 10
-    args.update_step_test = 20
+    args.update_step = 5
+    args.update_step_test = 10
     args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     # args.device = torch.device('cpu')
     return args
@@ -33,11 +33,11 @@ def get_network_structure(args):
     return config
 
 
-def get_dataset(args):
+def get_dataset(args, **kwargs):
     problem_dim = args.problem_dim
     n_problem = args.train_test
     spt_qry = (args.k_spt, args.k_qry)
-    dataset = create_dataset(problem_dim, n_problem=n_problem, spt_qry=spt_qry)
+    dataset = create_dataset(problem_dim, n_problem=n_problem, spt_qry=spt_qry, **kwargs)
     return dataset
 
 

main.py
@@ -116,14 +116,16 @@ def main():
     # see Sol.__init__ for more information
     args = get_args()
     network_structure = get_network_structure(args)
-    dataset = get_dataset(args)
+    dataset = get_dataset(args, normalize_targets=True)
     sol = Sol(dataset, args, network_structure)
     train_loss = sol.train(explicit=5)
     test_res, test_loss = sol.test()
-    test_res = test_res.cpu().detach().numpy()
-    print(np.sum(np.abs(test_res.flatten() - dataset[1][3].flatten())))
-
     print(f'Test loss: {test_loss:.4f}')
+
+    args.test_update_step = 30
+    sol = Sol(dataset, args, network_structure)
+    random_res, random_loss = sol.test()
+    print(f'Random loss: {random_loss:.4f}')
 
 
 if __name__ == '__main__':

maml_mod/meta.py
@@ -128,13 +128,14 @@ class Meta(nn.Module):
 
         return loss_q.item()
 
-    def finetunning(self, x_spt, y_spt, x_qry, y_qry):
+    def finetunning(self, x_spt, y_spt, x_qry, y_qry, retrun_single_lose=True):
         """
 
         :param x_spt:   [setsz, c_, h, w]
         :param y_spt:   [setsz]
         :param x_qry:   [querysz, c_, h, w]
         :param y_qry:   [querysz]
+        :param retrun_single_lose: if True, return a single loss, else return a list of losses
         :return:
         """
         loss_func = F.mse_loss
@@ -164,6 +165,7 @@ class Meta(nn.Module):
         loss = loss_func(logits, y_spt)
         grad = torch.autograd.grad(loss, net.parameters())
         fast_weights = list(map(lambda p: p[1] - self.update_lr * p[0], zip(grad, net.parameters())))
+        losses = []
 
         for k in range(1, self.update_step_test):
             # 1. run the i-th task and compute loss for k=1~K-1
@@ -176,9 +178,9 @@ class Meta(nn.Module):
 
             logits_q = net(x_qry, fast_weights, bn_training=True)
             # loss_q will be overwritten and just keep the loss_q on last update step.
-            loss_q = loss_func(logits_q, y_qry)
+            losses.append(loss_func(logits_q, y_qry).item())
 
         del net
 
-        return loss_q.item(), logits_q
+        return losses[-1] if retrun_single_lose else losses, logits_q
 